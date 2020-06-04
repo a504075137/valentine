d0376817eced12bffa1abc018bfae2b5b59e9e3e
luotianchun <luotianchun@corp.netease.com>
2020-06-04 11:20:40 +0800

    兼容已签到情况

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,27 +2,75 @@
   <div class="page-login">
     <header class="header">
       {{['登录飞智帐号','注册'][index]}}
-      <div class="back" v-if="index ==1" @click="index = 0"></div>
+      <div
+        class="back"
+        v-if="index ==1"
+        @click="index = 0"
+      ></div>
     </header>
-    <section class="content" v-if="index ==0">
-      <input type="text" v-input placeholder="请输入您的手机号/邮箱.." v-model="phone" />
-      <input type="text" v-input placeholder="请输入密码.." v-model="password" />
-      <div class="submit" @click="login">登录</div>
+    <section
+      class="content"
+      v-if="index ==0"
+    >
+      <input
+        type="text"
+        v-input
+        placeholder="请输入您的手机号/邮箱.."
+        v-model="phone"
+      />
+      <input
+        type="text"
+        v-input
+        placeholder="请输入密码.."
+        v-model="password"
+      />
+      <div
+        class="submit"
+        @click="login"
+      >登录</div>
     </section>
-    <section class="content regist" v-else>
+    <section
+      class="content regist"
+      v-else
+    >
       <!--      <input type="text" v-input placeholder="请输入您的昵称.." v-model="register.name" />-->
-      <input type="text" v-input placeholder="请输入您的手机号.." v-model="register.phone" />
+      <input
+        type="text"
+        v-input
+        placeholder="请输入您的手机号.."
+        v-model="register.phone"
+      />
       <div class="sms">
-        <input type="text" v-input placeholder="请输入手机验证码.." v-model="register.code" />
-        <div class="sms-btn btn-text" @click="sendSms">{{ sending ? secondNum : "获取验证码" }}</div>
+        <input
+          type="text"
+          v-input
+          placeholder="请输入手机验证码.."
+          v-model="register.code"
+        />
+        <div
+          class="sms-btn btn-text"
+          @click="sendSms"
+        >{{ sending ? secondNum : "获取验证码" }}</div>
       </div>
 
-      <input type="text" v-input placeholder="请输入密码（8-16位）.." v-model="register.password" />
+      <input
+        type="text"
+        v-input
+        placeholder="请输入密码（8-16位）.."
+        v-model="register.password"
+      />
       <!--      <input type="text" v-input placeholder="请再次输入密码.." v-model="register.repasswd" />-->
-      <div class="submit" @click="sendRegister">立即注册</div>
+      <div
+        class="submit"
+        @click="sendRegister"
+      >立即注册</div>
     </section>
 
-    <div class="register" v-if="index === 0" @click="goRegister">
+    <div
+      class="register"
+      v-if="index === 0"
+      @click="goRegister"
+    >
       <div class="desc">还没有账号？</div>
       <a href="#">立即注册</a>
     </div>
@@ -38,208 +86,201 @@
 let seconds = 1000 * 60;
 let registFlag = false;
 export default {
-    name: 'login',
-    meta: {
-        cn: '登录注册'
-    },
-    data () {
-        return {
-            index:0,
-            phone:null,
-            password:null,
-            register:{
-                phone:null,
-                code:null,
-                password:null,
-                repasswd:null,
-                name:null
-            },
-            secondNum: "",
-            sending: false,
-            timer: null,
+  name: "login",
+  meta: {
+    cn: "登录注册"
+  },
+  data() {
+    return {
+      index: 0,
+      phone: null,
+      password: null,
+      register: {
+        phone: null,
+        code: null,
+        password: null,
+        repasswd: null,
+        name: null
+      },
+      secondNum: "",
+      sending: false,
+      timer: null
+    };
+  },
+  created() {},
+  mounted() {},
+  methods: {
+    async login() {
+      if (this._check()) {
+        const obj = {
+          email: this.phone,
+          password: this.password
         };
+        try {
+          this.$loading.show();
+          const result = await this.$api.login(obj);
+          this.$bus.isLogin = true;
+          if (this.$bus.loginStatus === "mark") {
+            await this.$parent.mark();
+            await this.$api.boot({ activityId: this.$bus.activityId });
+            this.$emit("close");
+            return;
+          } else if (this.$storage.load("loginStatus")) {
+            await this.$parent.reMark(this.$storage.load("loginStatus"));
+            this.$storage.save("loginStatus", "");
+            await this.$api.boot({ activityId: this.$bus.activityId });
+            this.$emit("close");
+            return;
+          }
+          await this.$api.boot({ activityId: this.$bus.activityId });
+          this.$toast("登录成功");
+          this.$loading.hide();
+          // this.$router.replace("home");
+          this.$emit("close");
+        } catch (error) {
+          console.log(error);
+          this.$loading.hide();
+          if (error.err == 1) {
+            this.$toast({ message: "用户不存在" });
+          } else if (error.err == 2) {
+            this.$toast({ message: "密码错误" });
+          } else if (error.err == 3) {
+            this.$toast({ message: "安全问题错误" });
+          }
+        }
+      }
     },
-    created () {
-
+    _check() {
+      let err = "";
+      if (!this.phone) {
+        err = "请填写手机号";
+      } else if (!this.password) {
+        err = "请填写密码";
+      }
+      err && this.$toast({ message: err });
+      return !err;
     },
-    mounted () {
-
+    goRegister() {
+      this.index = 1;
     },
-    methods: {
-        async login(){
-            if(this._check()){
-                const obj = {
-                    email:this.phone,password:this.password
-                };
-                try {
-                    this.$loading.show();
-                    const result = await this.$api.login(obj);
-                    this.$bus.isLogin = true;
-                    if(this.$bus.loginStatus === 'mark'){
-                        await this.$parent.mark();
-                        this.$emit("close");
-                        return;
-                    }else if(this.$storage.load('loginStatus')){
-                        await this.$parent.reMark(this.$storage.load('loginStatus'));
-                        this.$storage.save('loginStatus','');
-                        await this.$api.boot({activityId:this.$bus.activityId});
-                        this.$emit("close");
-                        return;
-                    }
-                    await this.$api.boot({activityId:this.$bus.activityId});
-                    this.$toast('登录成功');
-                    this.$loading.hide();
-                    // this.$router.replace("home");
-                    this.$emit("close");
+    async sendSms() {
+      if (!this.register.phone) {
+        this.$toast("手机号不能为空");
+        return;
+      }
+      if (this.sending) {
+        this.$toast("请稍后再试");
+        return;
+      }
+      try {
+        this.$loading.show();
+        let result = await this.$api.sendCode({ mobile: this.register.phone });
+        this.$loading.hide();
+        result.err == 0 && this.$toast("发送验证码成功");
 
-                } catch (error) {
-                    console.log(error);
-                    this.$loading.hide();
-                    if(error.err == 1){
-                        this.$toast({message:"用户不存在"});
-                    }else if(error.err == 2){
-                        this.$toast({message:"密码错误"});
-                    }else if(error.err == 3){
-                        this.$toast({message:"安全问题错误"});
-                    }
-                }
+        if (result.err == 20001) {
+          this.$toast("手机号码不合法");
+        } else if (result.err == 20002) {
+          this.$toast("号码已被注册");
+        } else if (result.err == 20003) {
+          this.$toast("短信发送失败");
+        } else if (result.err == 20008) {
+          this.$toast("图形验证码错误");
+        } else {
+          this.sending = true;
+          this.secondNum = seconds / 1000 + "s";
+          this.timer = setInterval(() => {
+            if (seconds <= 0) {
+              this.sending = false;
+              seconds = 1000 * 60;
+              clearInterval(this.timer);
+              this.timer = null;
             }
-
-
-        },
-        _check() {
-            let err = "";
-            if (!this.phone) {
-                err = "请填写手机号";
-            } else if (!this.password) {
-                err = "请填写密码";
+            seconds -= 1000;
+            this.secondNum = seconds / 1000 + "s";
+          }, 1000);
+        }
+      } catch (error) {
+        this.$loading.hide();
+        console.log(error);
+        this.$toast("服务器开小差了，稍后再试试吧");
+      }
+    },
+    async sendRegister() {
+      if (this._checkRegist()) {
+        if (registFlag) {
+          return;
+        }
+        registFlag = true;
+        const result = await this.$api.register({
+          code: this.register.code,
+          mobile: this.register.phone,
+          password: this.register.password
+          // , username: this.register.name
+        });
+        switch (result.err) {
+          case 0:
+            this.$api.injectJwt(result.jwt);
+            this.$storage.save("jwt", result.jwt);
+            this.$bus.isLogin = true;
+            if (this.$bus.loginStatus === "mark") {
+              await this.$parent.mark();
+              this.$emit("close");
+              return;
+            } else if (this.$storage.load("loginStatus")) {
+              await this.$parent.reMark(this.$storage.load("loginStatus"));
+              this.$storage.save("loginStatus", "");
+              await this.$api.boot({ activityId: this.$bus.activityId });
+              this.$emit("close");
+              return;
             }
-            err && this.$toast({ message: err });
-            return !err;
-        },
-        goRegister(){
-            this.index = 1;
-        },
-        async sendSms() {
-            if (!this.register.phone) {
-                this.$toast("手机号不能为空");
-                return;
-            }
-            if (this.sending) {
-                this.$toast("请稍后再试");
-                return;
-            }
-            try {
-                this.$loading.show();
-                let result = await this.$api.sendCode({ mobile: this.register.phone });
-                this.$loading.hide();
-                result.err == 0  && this.$toast("发送验证码成功");
-
-                if(result.err == 20001 ){
-                    this.$toast("手机号码不合法");
-                }else if(result.err == 20002 ){
-                    this.$toast("号码已被注册");
-                }else if(result.err == 20003){
-                    this.$toast("短信发送失败");
-                }else if(result.err == 20008){
-                    this.$toast("图形验证码错误");
-                }else{
-                    this.sending = true;
-                    this.secondNum = seconds / 1000 + "s";
-                    this.timer = setInterval(() => {
-                        if (seconds <= 0) {
-                            this.sending = false;
-                            seconds = 1000 * 60;
-                            clearInterval(this.timer);
-                            this.timer = null;
-                        }
-                        seconds -= 1000;
-                        this.secondNum = seconds / 1000 + "s";
-                    }, 1000);
-                }
-
-            } catch (error) {
-                this.$loading.hide();
-                console.log(error);
-                this.$toast("服务器开小差了，稍后再试试吧");
-            }
-        },
-        async sendRegister(){
-
-            if(this._checkRegist()){
-                if(registFlag){
-                    return;
-                }
-                registFlag = true;
-                const result = await this.$api.register({  code: this.register.code,
-                    mobile: this.register.phone,
-                    password: this.register.password
-                    // , username: this.register.name
-                });
-                switch(result.err){
-                case 0:
-                    this.$api.injectJwt(result.jwt);
-                    this.$storage.save('jwt', result.jwt);
-                    this.$bus.isLogin = true;
-                    if(this.$bus.loginStatus === 'mark'){
-                        await this.$parent.mark();
-                        this.$emit("close");
-                        return;
-                    }else if(this.$storage.load('loginStatus')){
-                        await this.$parent.reMark(this.$storage.load('loginStatus'));
-                        this.$storage.save('loginStatus','');
-                        await this.$api.boot({activityId:this.$bus.activityId});
-                        this.$emit("close");
-                        return;
-                    }
-                    await this.$api.boot({activityId:this.$bus.activityId});
-                    this.$toast('注册成功');
-                    // this.$router.replace("home");
-                    this.$emit("close");
-                    break;
-                case 20001:
-                    this.$toast('手机号码不合法');
-                    break;
-                case 20002:
-                    this.$toast('号码已被注册');
-                    break;
-                case 20004:
-                    this.$toast('验证码错误');
-                    break;
-                case 20005:
-                    this.$toast('用户名被占用');
-                    break;
-                case 20006:
-                    this.$toast('注册失败');
-                    break;
-                }
-                registFlag = false;
-
-            }
-        },
-        _checkRegist() {
-            let err = "";
-            if (!this.register.phone) {
-                err = "请填写手机号";
-            }
-            // else if (!this.register.name) {
-            //     err = "请填写昵称";
-            // }
-            else if (!this.register.code) {
-                err = "请填写验证码";
-            } else if (!this.register.password) {
-                err = "请填写密码";
-            }
-            // else if (!this.register.repasswd) {
-            //     err = "请填写再次输入密码";
-            // }
-            // else if (this.register.repasswd !== this.register.password) {
-            //     err = "两次密码不一致";
-            // }
-            err && this.$toast({ message: err });
-            return !err;
-        },
+            await this.$api.boot({ activityId: this.$bus.activityId });
+            this.$toast("注册成功");
+            // this.$router.replace("home");
+            this.$emit("close");
+            break;
+          case 20001:
+            this.$toast("手机号码不合法");
+            break;
+          case 20002:
+            this.$toast("号码已被注册");
+            break;
+          case 20004:
+            this.$toast("验证码错误");
+            break;
+          case 20005:
+            this.$toast("用户名被占用");
+            break;
+          case 20006:
+            this.$toast("注册失败");
+            break;
+        }
+        registFlag = false;
+      }
+    },
+    _checkRegist() {
+      let err = "";
+      if (!this.register.phone) {
+        err = "请填写手机号";
+      }
+      // else if (!this.register.name) {
+      //     err = "请填写昵称";
+      // }
+      else if (!this.register.code) {
+        err = "请填写验证码";
+      } else if (!this.register.password) {
+        err = "请填写密码";
+      }
+      // else if (!this.register.repasswd) {
+      //     err = "请填写再次输入密码";
+      // }
+      // else if (this.register.repasswd !== this.register.password) {
+      //     err = "两次密码不一致";
+      // }
+      err && this.$toast({ message: err });
+      return !err;
     }
+  }
 };
 </script>
 
